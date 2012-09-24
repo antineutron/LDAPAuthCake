301ededa00d24be0663ccd322c672e6411b261a0
Andy Newton <amn@ecs.soton.ac.uk>
2012-09-24 01:27:39 +0100

    LDAP auth basically working, with a slight bug...
    Authentication against LDAP now works, and will auto-create a user object
    with the correct details if it doesn't already exist.
    
    If it fails, it's possible to fall back to form auth or similar.
    
    Currently there is a bug whereby login works fine but tells the user it failed. This is inconvenient.

--- Controller/Component/Auth/LDAPAuthenticate.php
+++ Controller/Component/Auth/LDAPAuthenticate.php
@@ -5,42 +5,7 @@ App::uses('BaseAuthenticate', 'Controller/Component/Auth');
 class LDAPAuthenticate extends BaseAuthenticate {
 
 
-    /**
-     * @param CakeRequest $request The request that contains login information.
-     * @param CakeResponse $response Unused response object.
-     * @return mixed.  False on login failure.  An array of User data on success.
-     */
-    public function authenticate(CakeRequest $request, CakeResponse $response) {
-
-        $user_field = $this->settings['fields']['username'];
-        $pass_field = $this->settings['fields']['password'];
-
-        // Definitely not authenticated if we haven't got the request data...
-        if(!isset($request->data['User'])){
-            return false;
-        }
-
-        // We need both the username and password fields present
-        $submitted_details = $request->data['User'];
-        if(!isset($submitted_details[$user_field]) || !isset($submitted_details[$pass_field])){
-            return false;
-        }
-
-        // Make sure they're actually strings
-        $username = $submitted_details[$user_field];
-        $password = $submitted_details[$pass_field];
-        if( !is_string($username) || !is_string($password) ){
-            return false;
-        }
-        
-        // Perform LDAP lookup to find the user object, if possible
-        return $this->_findUser($username, $password);
-
-    }
-
-    
-    protected function _findUser($username, $password){
-        
+    private function _ldapConnect(){
 
         $ldap_connection = ldap_connect($this->settings['ldap_url']);
         if(!$ldap_connection){
@@ -53,56 +18,137 @@ class LDAPAuthenticate extends BaseAuthenticate {
             throw new CakeException("Could not bind to LDAP authentication server - check your bind DN and password");
         }
 
+        return $ldap_connection;
+    }
+
+
+    /**
+     * @param CakeRequest $request The request that contains login information.
+     * @param CakeResponse $response Unused response object.
+     * @return mixed.  False on login failure.  An array of User data on success.
+     */
+    public function authenticate(CakeRequest $request, CakeResponse $response) {
+
+///        $this->log("[LDAPAuthCake.authenticate] Authentication started", 'ldapauth');
+
+        // This will probably be cn or an email field to search for
+        $fields = $this->settings['form_fields'];
+        $user_field = $fields['username'];
+        $pass_field = $fields['password'];
+
+        // Definitely not authenticated if we haven't got the request data...
+        if(!isset($request->data['User'])){
+///            $this->log("[LDAPAuthCake.authenticate] No request data, cannot authenticate", 'ldapauth');
+            return false;
+        }
+
+        // We need to know the username, or email, or some other unique ID
+        $submitted_details = $request->data['User'];
+        if(!isset($submitted_details[$user_field])){
+///            $this->log("[LDAPAuthCake.authenticate] No username supplied, cannot authenticate", 'ldapauth');
+            return false;
+        }
+
+        // Make sure it's a valid string...
+        $username = $submitted_details[$user_field];
+        if( !is_string($username) ){
+///            $this->log("[LDAPAuthCake.authenticate] Invalid username, cannot authenticate", 'ldapauth');
+            return false;
+        }
+
+        // Make sure they gave us a password too...
+        $password = $submitted_details[$pass_field];
+        if( !is_string($password) || empty($password) ){
+            return false;
+        }
+
         // Get the ldap_filter setting and insert the username
         $ldap_filter = $this->settings['ldap_filter'];
         $ldap_filter = preg_replace('/%USERNAME%/', $username, $ldap_filter);
 
-        $results = ldap_search($ldap_connection, $this->settings['ldap_base_dn'], $ldap_filter, array('dn'), 1, 1);
+        // We'll get the DN by default but we also want the useful bits we can map
+        // to our own database details
+        $attribs = array_keys($this->settings['ldap_to_user']);
+
+        // Connect to LDAP server and search for the user object
+        $ldap_connection = $this->_ldapConnect();
+        $results = ldap_search($ldap_connection, $this->settings['ldap_base_dn'], $ldap_filter, $attribs, 0, 1);
 
         // Failed to find user details, not authenticated.
-        if(!$results || !ldap_count_entries($ldap_connection, $results)){
+        if(!$results || ldap_count_entries($ldap_connection, $results) == 0){
+///            $this->log("[LDAPAuthCake.authenticate] Could not find user $username", 'ldapauth');
             return false;
         }
 
-        // Find the user's DN so we can re-bind with their account
-        $data = ldap_get_entries($ldap_connection, $results);
-        $user_dn = $data[0]['dn'];
+        // Got multiple results, sysadmin did something wrong!
+        if(ldap_count_entries($ldap_connection, $results) > 1){
+///            $this->log("[LDAPAuthCake.authenticate] Multiple LDAP results for $username", 'ldapauth');
+            return false;
+        }
+
+        // Found the user! Get their details
+        $ldap_user = ldap_get_entries($ldap_connection, $results);
+        $ldap_user = $ldap_user[0];
+
+        $results = array();
+        
+        // Get a list of DB fields mapped to values from LDAP
+        foreach ($this->settings['ldap_to_user'] as $ldap_field => $db_field){
+            
+            $value = $ldap_user[strtolower($ldap_field)][0];
+            $results[$db_field] = $value;
+            
+            // If this is the unique username field, overwrite it for lookups.
+            if($db_field == $fields['username']){
+                $username = strtolower($value);
+            }
+        }
+
 
         // Now try to re-bind as that user
-        $bind = ldap_bind($ldap_connection, $user_dn, $password);
+        $bind = ldap_bind($ldap_connection, $ldap_user['dn'], $password);
 
+        // If the password didn't work, bomb out
         if(!$bind){
             return false;
         }
 
-        // Now find the user object in the database
+        // Look up the user in our DB based on the unique field (username, email or whatever)
         // NB this is nicked from BaseAuthenticate but without the password check
         $userModel = $this->settings['userModel'];
         list($plugin, $model) = pluginSplit($userModel);
+        
 
-        $fields = $this->settings['fields'];
-
+        // Case-insensitive matching for this...
         $conditions = array(
-            $model . '.' . $fields['username'] => $username,
+            'LOWER(' . $model . '.' . $fields['username'] . ')' => strtolower($username),
         );
 
-        if (!empty($this->settings['scope'])) {
-            $conditions = array_merge($conditions, $this->settings['scope']);
-        }
 
-        $result = ClassRegistry::init($userModel)->find('first', array(
+        $db_user = ClassRegistry::init($userModel)->find('first', array(
             'conditions' => $conditions,
-            'recursive' => (int)$this->settings['recursive']
+            'recursive'  => false
         ));
 
-        if (empty($result) || empty($result[$model])) {
-            // TODO this is where we would auto-create a new user object, if we could...
-            return false;
+        // If we couldn't find them in the database, create a new DB entry
+        if (empty($db_user) || empty($db_user[$model])) {
+///            $this->log("[LDAPAuthCake.authenticate] Could not find a database entry for $username", 'ldapauth');
+
+            $results = array_merge($results, $this->settings['defaults']);
+            if (!ClassRegistry::init($userModel)->save($results)) {
+                echo "Failed to save new user\n"; print_r($results); print_r($username);
+                return false;
+            }
+
+            $id = ClassRegistry::init($userModel)->getLastInsertID();
+            $db_user = ClassRegistry::init($userModel)->findById($id);
         }
 
-        unset($result[$model][$fields['password']]);
-        return $result[$model];
-       
+        // Ensure there's nothing in the password field
+        unset($db_user[$model][$fields['password']]);
+
+        // ...and return the user object.
+        return $db_user[$model];
     }
 
 }
